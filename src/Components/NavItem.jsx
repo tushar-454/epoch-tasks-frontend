83f7e5edae02e385a298ac1049b116b9392620c4
tushar-454 <imtushar454@gmail.com>
2023-12-21 11:01:50 +0600

    Header nav active added successfully

--- src/Components/NavItem.jsx
+++ src/Components/NavItem.jsx
@@ -1,14 +1,16 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const NavItem = ({ navItem }) => {
   return (
-    <Link
-      className='flex items-center gap-x-2 font-medium text-gray-500 transition-all hover:text-froly-600 sm:border-s sm:border-gray-300 sm:my-6 sm:ps-6 dark:border-gray-700 dark:text-gray-400 dark:hover:text-froly-500'
+    <NavLink
+      className={`flex items-center gap-x-2 font-medium text-gray-500 transition-all hover:text-froly-600 sm:border-s sm:border-gray-300 sm:my-6 sm:ps-6 dark:border-gray-700 dark:text-gray-400 dark:hover:text-froly-500 ${({
+        isActive,
+      }) => (isActive ? 'active' : undefined)}`}
       to={navItem.path}
     >
       {navItem.icon}
       {navItem.route}
-    </Link>
+    </NavLink>
   );
 };
 
