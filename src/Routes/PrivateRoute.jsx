049aa8108390dbb46c3f1cf1a0b0ba3d2887a596
tushar-454 <imtushar454@gmail.com>
2023-12-21 16:53:02 +0600

    public and private route dark mode class added

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -6,22 +6,22 @@ const PrivateRoute = ({ children }) => {
   if (loading) {
     return (
       <>
-        <div className='flex'>
-          <div className='w-[240px] hidden lg:block'>
-            <p className='w-full h-12 my-2 bg-gray-300 rounded-lg mx-auto'></p>
-            <p className='w-full h-12 my-2 bg-gray-300 rounded-lg mx-auto'></p>
-            <p className='w-full h-12 my-2 bg-gray-300 rounded-lg mx-auto'></p>
-            <p className='w-full h-12 my-2 bg-gray-300 rounded-lg mx-auto'></p>
-            <p className='w-full h-12 my-2 bg-gray-300 rounded-lg mx-auto'></p>
-            <p className='w-full h-12 my-2 bg-gray-300 rounded-lg mx-auto'></p>
-            <p className='w-full h-12 my-2 bg-gray-300 rounded-lg mx-auto'></p>
+        <div className='flex bg-white dark:bg-gray-800 min-h-screen'>
+          <div className='w-[240px] hidden lg:block border-r p-1'>
+            <p className='w-full h-12 my-2 bg-gray-300 dark:bg-gray-700 rounded-lg mx-auto'></p>
+            <p className='w-full h-12 my-2 bg-gray-300 dark:bg-gray-700 rounded-lg mx-auto'></p>
+            <p className='w-full h-12 my-2 bg-gray-300 dark:bg-gray-700 rounded-lg mx-auto'></p>
+            <p className='w-full h-12 my-2 bg-gray-300 dark:bg-gray-700 rounded-lg mx-auto'></p>
+            <p className='w-full h-12 my-2 bg-gray-300 dark:bg-gray-700 rounded-lg mx-auto'></p>
+            <p className='w-full h-12 my-2 bg-gray-300 dark:bg-gray-700 rounded-lg mx-auto'></p>
+            <p className='w-full h-12 my-2 bg-gray-300 dark:bg-gray-700 rounded-lg mx-auto'></p>
           </div>
           <div className='flex-grow p-4'>
-            <p className='w-[340px] h-12 my-5 bg-gray-300 rounded-lg mx-auto'></p>
+            <p className='w-[340px] h-12 my-5 bg-gray-300 dark:bg-gray-700 rounded-lg mx-auto'></p>
             <div className='flex flex-col lg:flex-row gap-10 justify-between items-center'>
-              <div className='w-full lg:w-1/3 h-40 bg-gray-300 rounded-lg'></div>
-              <div className='w-full lg:w-1/3 h-40 bg-gray-300 rounded-lg'></div>
-              <div className='w-full lg:w-1/3 h-40 bg-gray-300 rounded-lg'></div>
+              <div className='w-full lg:w-1/3 h-40 bg-gray-300 dark:bg-gray-700 rounded-lg'></div>
+              <div className='w-full lg:w-1/3 h-40 bg-gray-300 dark:bg-gray-700 rounded-lg'></div>
+              <div className='w-full lg:w-1/3 h-40 bg-gray-300 dark:bg-gray-700 rounded-lg'></div>
             </div>
           </div>
         </div>
--- src/Routes/PublicRoute.jsx
+++ src/Routes/PublicRoute.jsx
@@ -11,21 +11,21 @@ const PublicRoute = ({ children }) => {
             <div className='mt-7 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700'>
               <div className='p-4 sm:p-7'>
                 <div className='text-center'>
-                  <h1 className='w-20 h-4 bg-gray-300 rounded-full mx-auto'></h1>
-                  <p className='w-full h-8 my-5 bg-gray-300 rounded-full mx-auto'></p>
+                  <h1 className='w-20 h-4 bg-gray-300 dark:bg-gray-700 rounded-full mx-auto'></h1>
+                  <p className='w-full h-8 my-5 bg-gray-300 dark:bg-gray-700 rounded-full mx-auto'></p>
                 </div>
 
                 <div className='mt-5'>
-                  <div className='w-20 h-4 bg-gray-300 rounded-full mx-auto'></div>
+                  <div className='w-20 h-4 bg-gray-300 dark:bg-gray-700 rounded-full mx-auto'></div>
                   <form>
                     <div className='grid gap-y-4'>
-                      <p className='w-20 h-4 bg-gray-300 rounded-full'></p>
-                      <p className='w-full h-10 bg-gray-300 rounded-full mx-auto'></p>
-                      <p className='w-20 h-4 bg-gray-300 rounded-full'></p>
-                      <p className='w-full h-10 bg-gray-300 rounded-full mx-auto'></p>
-                      <button className='w-full h-12 my-4 bg-gray-300 rounded-full mx-auto'></button>
+                      <p className='w-20 h-4 bg-gray-300 dark:bg-gray-700 rounded-full'></p>
+                      <p className='w-full h-10 bg-gray-300 dark:bg-gray-700 rounded-full mx-auto'></p>
+                      <p className='w-20 h-4 bg-gray-300 dark:bg-gray-700 rounded-full'></p>
+                      <p className='w-full h-10 bg-gray-300 dark:bg-gray-700 rounded-full mx-auto'></p>
+                      <button className='w-full h-12 my-4 bg-gray-300 dark:bg-gray-700 rounded-full mx-auto'></button>
                     </div>
-                    <p className='w-32 h-4 bg-gray-300 rounded-full mx-auto'></p>
+                    <p className='w-32 h-4 bg-gray-300 dark:bg-gray-700 rounded-full mx-auto'></p>
                   </form>
                 </div>
               </div>
