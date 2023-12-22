35dcf8d04573e9363a903a56ec63a6ccff41091b
tushar-454 <imtushar454@gmail.com>
2023-12-22 16:40:17 +0600

    trash task show in tab and restore and permanently delete function added

--- src/Components/TrashTaskCard.jsx
+++ src/Components/TrashTaskCard.jsx
@@ -1,18 +1,63 @@
-const TrashTaskCard = () => {
+import useAxios from '../Hook/useAxios';
+import Toast from '../Utils/Toast/Toast';
+
+const TrashTaskCard = ({ userTrashTask, refetch }) => {
+  const { _id, title, description, deadline } = userTrashTask;
+  const axios = useAxios();
+  // handle transfar task from trash
+  const handleRestoreTask = async (id) => {
+    try {
+      const res = await axios.patch(`/task/trash/${id}`, {
+        trashStatus: false,
+      });
+      if (res.data.message === 'success') {
+        Toast('Task is restore now !', 'info');
+        refetch();
+      }
+    } catch (error) {
+      Toast('There was an error !', 'error');
+    }
+  };
+  // handle delete task permanently
+  const handleDeleteTask = async (id) => {
+    try {
+      const res = await axios.delete(`/task/remove/${id}`);
+      if (res.data.message === 'success') {
+        Toast('Task is permanently deleted !', 'info');
+        refetch();
+      }
+    } catch (error) {
+      Toast('There was an error !', 'error');
+    }
+  };
+
   return (
-    <div className='p-2 bg-froly-50 border-l-8 rounded-md border-red-500'>
-      <h1 className='text-2xl font-bold'>Title</h1>
-      <p className='text-xl'>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore rem ad
-        quaerat! Pariatur, eveniet explicabo?
+    <div className='flex flex-col p-2 bg-froly-50 border-l-8 rounded-md border-red-500'>
+      <h1 className='text-2xl font-bold'>
+        {title.length > 23 ? `${title.slice(0, 22)}...` : title}
+      </h1>
+      <p className='text-xl flex-grow'>
+        {description.length > 100
+          ? `${description.slice(0, 100)}...`
+          : description}
       </p>
-      <p className='my-4'>12/22/2023 - 12-25-2023</p>
-      <p className='p-1 px-2 rounded bg-green-200 inline-block mr-3 cursor-pointer transition-all hover:bg-green-300 active:bg-green-400'>
-        Restore
+      <p className='my-4'>
+        <b>Deadline:-</b> {deadline}
       </p>
-      <p className='p-1 px-2 rounded bg-red-200 inline-block mr-3 cursor-pointer transition-all hover:bg-red-300 active:bg-red-400'>
-        Delete
-      </p>
+      <div className='flex gap-1'>
+        <p
+          onClick={() => handleRestoreTask(_id)}
+          className='p-1 px-2 rounded bg-green-200 inline-block mr-3 cursor-pointer transition-all hover:bg-green-300 active:bg-green-400'
+        >
+          Restore
+        </p>
+        <p
+          onClick={() => handleDeleteTask(_id)}
+          className='p-1 px-2 rounded bg-red-200 inline-block mr-3 cursor-pointer transition-all hover:bg-red-300 active:bg-red-400'
+        >
+          Delete
+        </p>
+      </div>
     </div>
   );
 };
