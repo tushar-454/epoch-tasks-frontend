6e5365385d84fc9f5f0f905b41c6f1c571e2785f
tushar-454 <imtushar454@gmail.com>
2023-12-22 15:56:41 +0600

    manage task section task goto trash function done

--- src/Components/ManageTaskCard.jsx
+++ src/Components/ManageTaskCard.jsx
@@ -1,18 +1,47 @@
-const ManageTaskCard = () => {
+import useAxios from '../Hook/useAxios';
+import Toast from '../Utils/Toast/Toast';
+
+const ManageTaskCard = ({ userTask, refetch }) => {
+  const axios = useAxios();
+  const { _id, title, description, deadline } = userTask;
+  // handle transfar task in trash
+  const handleTaskTrash = async (id) => {
+    try {
+      const res = await axios.patch(`/task/trash/${id}`, {
+        trashStatus: true,
+      });
+      if (res.data.message === 'success') {
+        Toast('Task is trash now !', 'info');
+        refetch();
+      }
+    } catch (error) {
+      Toast('There was an error !', 'error');
+    }
+  };
   return (
-    <div className='p-2 bg-sun-50 border-l-8 rounded-md border-sun-500'>
-      <h1 className='text-2xl font-bold'>Title</h1>
-      <p className='text-xl'>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore rem ad
-        quaerat! Pariatur, eveniet explicabo?
+    <div className='flex flex-col p-2 bg-sun-50 border-l-8 rounded-md border-sun-500'>
+      <h1 className='text-2xl font-bold'>
+        {title.length > 23 ? `${title.slice(0, 22)}...` : title}
+      </h1>
+      <p className='text-xl flex-grow'>
+        {description.length > 100
+          ? `${description.slice(0, 100)}...`
+          : description}
       </p>
-      <p className='my-4'>12/22/2023 - 12-25-2023</p>
-      <p className='p-1 px-2 rounded bg-blue-200 inline-block mr-3 cursor-pointer transition-all hover:bg-blue-300 active:bg-blue-400'>
-        Update
+      <p className='my-4'>
+        <b>Deadline:-</b> {deadline}
       </p>
-      <p className='p-1 px-2 rounded bg-red-200 inline-block mr-3 cursor-pointer transition-all hover:bg-red-300 active:bg-red-400'>
-        Trash
-      </p>
+      <div className='flex gap-1'>
+        <p className='p-1 px-2 rounded bg-blue-200 inline-block mr-3 cursor-pointer transition-all hover:bg-blue-300 active:bg-blue-400'>
+          Update
+        </p>
+        <p
+          onClick={() => handleTaskTrash(_id)}
+          className='p-1 px-2 rounded bg-red-200 inline-block mr-3 cursor-pointer transition-all hover:bg-red-300 active:bg-red-400'
+        >
+          Trash
+        </p>
+      </div>
     </div>
   );
 };
--- src/Pages/ManageTask.jsx
+++ src/Pages/ManageTask.jsx
@@ -1,17 +1,44 @@
+import { useQuery } from '@tanstack/react-query';
 import ManageTaskCard from '../Components/ManageTaskCard';
+import useAuth from '../Hook/useAuth';
+import useAxios from '../Hook/useAxios';
 import DashBoardNavItemTitle from '../Shared/DashBoardNavItemTitle';
-
+import Toast from '../Utils/Toast/Toast';
 const ManageTask = () => {
+  const { user } = useAuth();
+  const axios = useAxios();
+  const {
+    data: userTasks,
+    isLoading,
+    isError,
+    refetch,
+  } = useQuery({
+    queryKey: ['userTask'],
+    queryFn: async () => {
+      const res = await axios.get(`/task/get/${user?.email}`);
+      return res.data;
+    },
+  });
   return (
     <div className='w-full mx-auto'>
       <DashBoardNavItemTitle displayName='Manage Task' />
       <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5'>
-        <ManageTaskCard />
-        <ManageTaskCard />
-        <ManageTaskCard />
-        <ManageTaskCard />
-        <ManageTaskCard />
-        <ManageTaskCard />
+        {isLoading ? (
+          <span className='relative flex justify-center h-10 w-10'>
+            <span className='animate-ping absolute inline-flex h-full w-full rounded-full bg-froly-400 dark:bg-sun-400 opacity-75'></span>
+            <span className='relative inline-flex rounded-full h-10 w-10 bg-froly-500 dark:bg-sun-500'></span>
+          </span>
+        ) : isError ? (
+          <>{Toast('There was an error', 'error')}</>
+        ) : !isLoading && userTasks.length === 0 ? (
+          <>
+            <h1>No active task in your list.</h1>
+          </>
+        ) : (
+          userTasks?.map((userTask, index) => (
+            <ManageTaskCard key={index} userTask={userTask} refetch={refetch} />
+          ))
+        )}
       </div>
     </div>
   );
