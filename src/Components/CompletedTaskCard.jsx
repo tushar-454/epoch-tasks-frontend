40d672a6e666c979caafebd46de5503fac335028
tushar-454 <imtushar454@gmail.com>
2023-12-22 16:52:31 +0600

    completed task route user all completed task show done

--- src/Components/CompletedTaskCard.jsx
+++ src/Components/CompletedTaskCard.jsx
@@ -1,15 +1,32 @@
-const CompletedTaskCard = () => {
+const CompletedTaskCard = ({ completedTask }) => {
+  const { title, description, deadline, priroty } = completedTask;
   return (
-    <div className='p-2 bg-green-100 border-l-8 rounded-md border-green-500'>
-      <h1 className='text-2xl font-bold'>Title</h1>
-      <p className='text-xl'>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore rem ad
-        quaerat! Pariatur, eveniet explicabo?
+    <div className='flex flex-col p-2 bg-green-100 border-l-8 rounded-md border-green-500'>
+      <h1 className='text-2xl font-bold'>
+        {' '}
+        {title.length > 23 ? `${title.slice(0, 22)}...` : title}
+      </h1>
+      <p className='text-xl flex-grow'>
+        {description.length > 100
+          ? `${description.slice(0, 100)}...`
+          : description}
       </p>
-      <p className='my-4'>12/22/2023 - 12-25-2023</p>
-      <p className='p-1 px-2 rounded bg-green-200 inline-block mr-3'>Low</p>
-      <p className='p-1 px-2 rounded bg-yellow-200 inline-block mr-3'>Medium</p>
-      <p className='p-1 px-2 rounded bg-red-300 inline-block mr-3'>High</p>
+      <p className='my-4'>
+        <b>Deadline:-</b> {deadline}
+      </p>
+      <div>
+        {priroty === 'low' && (
+          <p className='p-1 px-2 rounded bg-green-200 inline-block mr-3'>Low</p>
+        )}
+        {priroty === 'medium' && (
+          <p className='p-1 px-2 rounded bg-yellow-200 inline-block mr-3'>
+            Medium
+          </p>
+        )}
+        {priroty === 'high' && (
+          <p className='p-1 px-2 rounded bg-red-300 inline-block mr-3'>High</p>
+        )}
+      </div>
     </div>
   );
 };
